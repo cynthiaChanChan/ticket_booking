9b24d122715b7f5720a92a426fd9d6827525ed57
cynthiaChanChan <mession01@gmail.com>
2019-07-09 14:28:21 +0800

    09.3 Making a Successful Charge

---
+++ app/billing/StripePaymentGateway.php
@@ -0,0 +1,23 @@
+<?php 
+namespace App\Billing;
+
+use Stripe\Charge;
+
+class StripePaymentGateway implements PaymentGateway
+{
+	private $apiKey;
+
+	public function __construct($apiKey)
+	{
+		$this->apiKey = $apiKey;
+	}
+
+	public function charge($amount, $token)
+	{
+		Charge::create([
+			'amount' => 2500,
+			'currency' => 'usd',
+			'source' => $token
+		], ['api_key' => $this->apiKey]);
+	}
+}
