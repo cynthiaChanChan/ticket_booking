62c69892e55e0049d68132ac4732771defc3e70d
cynthiaChanChan <mession01@gmail.com>
2019-07-12 14:23:08 +0800

    12.3 - Asserting Against View Data

--- app/Http/Controllers/ordersController.php
+++ app/Http/Controllers/ordersController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Order;
 use Illuminate\Http\Request;
 
 class ordersController extends Controller
 {
-    public function show() 
+    public function show($confirmationNumber) 
     {
-    	
+    	$order = Order::where("confirmation_number", $confirmationNumber)->first();
+    	return view("orders.show", ['order' => $order]);
     }
 }
