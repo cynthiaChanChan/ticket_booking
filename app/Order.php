a68aa83ca0849132eacffd14f84a2af192bf1e4d
cynthiaChanChan <mession01@gmail.com>
2019-07-17 12:23:56 +0800

    13.2 - 13.3 Stubbing the Interface

--- app/Order.php
+++ app/Order.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Ticket;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
@@ -11,6 +12,7 @@ class Order extends Model
     public static function forTickets($tickets, $email, $amount)
     {
         $order = self::create([
+            'confirmation_number' => app(OrderConfirmationNumberGenerator::class)->generate(),
             'email' => $email, 
             'amount' => $amount
         ]);
@@ -40,6 +42,7 @@ class Order extends Model
     public function toArray()
     {
         return [
+            'confirmation_number' => $this->confirmation_number,
             'email' => $this->email,
             'ticket_quantity' => $this->ticketQuantity(),
             'amount' => $this->amount
