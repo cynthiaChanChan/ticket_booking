478421d3b48829d52792a8666d1afce1ffe15bf2
cynthiaChanChan <mession01@gmail.com>
2019-07-17 13:45:38 +0800

    13.4 Confirmation Number Characteristics

---
+++ tests/Unit/RandomOrderConfirmationNumberGeneratorTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\RandomOrderConfirmationNumberGenerator;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RandomOrderConfirmationNumberGeneratorTest extends TestCase
+{
+	// Must be 24 characters long
+	// Can only contain uppercase letters and numbers
+	// Cannot contain ambiguous characters
+	// All order confirmation numbers must be unique
+	//
+	// ABCDEFGHJKLMNPQRSTUVWXYZ
+	// 23456789
+
+	/** @test */
+    function must_be_24_characters_long()
+    {
+    	$generator = new RandomOrderConfirmationNumberGenerator;
+
+    	$confirmationNumber = $generator->generate();
+
+    	$this->assertEquals(24, strlen($confirmationNumber));
+    }
+
+	/** @test */
+    function can_only_contain_uppercase_letters_and_numbers()
+    {
+    	$generator = new RandomOrderConfirmationNumberGenerator;
+
+    	$confirmationNumber = $generator->generate();
+
+    	$this->assertRegExp('/^[A-Z0-9]+$/', $confirmationNumber);
+    }
+
+	/** @test */
+    function cannot_contain_ambiguous_characters()
+    {
+    	$generator = new RandomOrderConfirmationNumberGenerator;
+
+    	$confirmationNumber = $generator->generate();
+
+    	$this->assertFalse(strpos($confirmationNumber, '1'));
+    	$this->assertFalse(strpos($confirmationNumber, 'I'));
+    	$this->assertFalse(strpos($confirmationNumber, '0'));
+    	$this->assertFalse(strpos($confirmationNumber, 'O'));
+    }
+}
