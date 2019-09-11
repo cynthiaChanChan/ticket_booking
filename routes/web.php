dcb611dfdd7e012d4efaa43553f95ad0e73ab40b
cynthiaChanChan <mession01@gmail.com>
2019-09-11 12:24:33 +0800

    17.1 the Login Endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-Route::get('/', function() {
-	return view('welcome');
-});
-
 Route::get('/concerts/{id}', 'concertController@show');
 
 Route::post('/concerts/{id}/orders', 'concertOrdersController@store');
 
 Route::get('/orders/{confirmationNumber}', 'ordersController@show');
+
+Route::post('/login', 'Auth\LoginController@login');
