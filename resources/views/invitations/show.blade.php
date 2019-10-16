dcc4a964b4d6f26b6a3e4e7537c22f209510012e
cynthiaChanChan <mession01@gmail.com>
2019-10-16 12:53:08 +0800

    Users can register with a valid invitation code

--- resources/views/invitations/show.blade.php
+++ resources/views/invitations/show.blade.php
@@ -7,6 +7,7 @@
 
             <form action="{{ url('register') }}" method="POST">
                 {{ csrf_field() }}
+                <input type="hidden" name="invitation_code" value="{{ $invitation->code }}">
                 <div class="login__item {{ $errors->first('email', 'has-error') }}">
                     <div class="login__icon-box">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M5 5a5 5 0 0 1 10 0v2A5 5 0 0 1 5 7V5zM0 16.68A19.9 19.9 0 0 1 10 14c3.64 0 7.06.97 10 2.68V20H0v-3.32z"></path></svg>
