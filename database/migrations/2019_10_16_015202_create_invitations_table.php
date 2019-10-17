197d75df7c454b78815af740394b8e21b70d15a2
cynthiaChanChan <mession01@gmail.com>
2019-10-17 13:49:43 +0800

    Create new invitations from CLI

--- database/migrations/2019_10_16_015202_create_invitations_table.php
+++ database/migrations/2019_10_16_015202_create_invitations_table.php
@@ -15,6 +15,7 @@ class CreateInvitationsTable extends Migration
     {
         Schema::create('invitations', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('email');
             $table->string('code');
             $table->integer('user_id')
                   ->unsigned()
