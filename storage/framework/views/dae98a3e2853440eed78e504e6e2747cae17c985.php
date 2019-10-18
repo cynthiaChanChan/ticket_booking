402465cd455ef15ddb3784c7c734bc035d905e1e
cynthiaChanChan <mession01@gmail.com>
2019-10-18 21:21:49 +0800

    29.8 Create concert invitation email view

---
+++ storage/framework/views/dae98a3e2853440eed78e504e6e2747cae17c985.php
@@ -0,0 +1,5 @@
+<p>Hey we would like to invite you to promote your concerts on Ticket_booking!</p>
+
+<p>Visit this link to register you account.</p>
+
+<a href='<?php echo e(url("invitations/{$invitation->code}")); ?>'><?php echo e(url("invitations/{$invitation->code}")); ?></a> <?php /**PATH C:\wamp64\www\ticket_booking\resources\views/emails/invitation-email.blade.php ENDPATH**/ ?>
